136efc89a0942b7bf9c9e3f7662e52278afe29b8
aria savana jing wen <156157165+Ariaa06@users.noreply.github.com>
2024-04-01 18:22:21 +0700

    Update dokter.php

--- app/Views/dokter.php
+++ app/Views/dokter.php
@@ -3,71 +3,79 @@
         <div class="container-fluid px-4">
             <h1 class="mt-4">Book A Doctor</h1>
             <ol class="breadcrumb mb-4">
-                <li class="breadcrumb-item"><a href="<?=base_url('home/dashboard')?>">Home</a></li>
+                <li class="breadcrumb-item"><a href="<?= base_url('home/dashboard') ?>">Home</a></li>
             </ol>
             <li>today time:
                 <?php echo date('H:i:s'); ?></li>
 
-                <?php if(session()->has('success')): ?>
-    <div class="alert alert-success" role="alert">
-        <?= session()->getFlashdata('success') ?>
-    </div>
-<?php endif; ?>
+            <?php if (session()->has('success')) : ?>
+                <div class="alert alert-success" role="alert">
+                    <?= session()->getFlashdata('success') ?>
+                </div>
+            <?php endif; ?>
 
-                <div class="card mb-4">
+            <div class="card mb-4">
 
-                    <?php if(session()->get('level')==1 || session()->get('level')==2) { ?>
-                        <a href="<?= base_url('home/tambah_dokter') ?>">
-                           <button class="btn btn-outline-primary">Add Doctor</button>
-                       <?php } ?>
-                </a>
+                <?php if (session()->get('level') == 1 || session()->get('level') == 2) { ?>
+                    <a href="<?= base_url('home/tambah_dokter') ?>">
+                        <button class="btn btn-outline-primary">Add Doctor</button>
+                    </a>
+                <?php } ?>
                 <div class="card-body">
-                 <table class="table table-hover">
-                    <thead>
-                      <tr>
-                        <th scope="col">no</th>
-                        <th scope="col">Nama Dokter</th>
-                        <th scope="col">Bidang</th>
-                        <th scope="col">Harga</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                  <?php
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">no</th>
+                                <th scope="col">Nama Dokter</th>
+                                <th scope="col">Bidang</th>
+                                <th scope="col">Harga</th>
+                                <th scope="col">Tanggal</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
 
-                  $no=1;
-                  foreach($manda as $erwin){
-                     ?>   
+                            $no = 1;
+                            foreach ($manda as $erwin) {
+                            ?>
 
-                     <tr>
-                        <td><?= $no++ ?></td>
-                        <td><?=$erwin->nama_dokter?></td>
-                        <td><?= $erwin ->bidang?></td>
-                        <td><?= $erwin ->harga?></td>
-                        <td><?= $erwin ->status?></td>
-                        <td>
-                            <?php if(session()->get('level')==1 || session()->get('level')==2) { ?>
+                                <tr>
+                                    <td><?= $no++ ?></td>
+                                    <td><?= $erwin->nama_dokter ?></td>
+                                    <td><?= $erwin->bidang ?></td>
+                                    <td><?= $erwin->harga ?></td>
+                                    <td>
+                                        <?php if ($erwin->status == 'Booked' && $erwin->booking_date) : ?>
+                                            <?= $erwin->booking_date ?>
+                                        <?php else : ?>
+                                            <?= date('Y-m-d') ?> <!-- Tanggal hari ini -->
+                                        <?php endif; ?>
+                                    </td>
+                                    <td><?= $erwin->status ?></td>
+                                    <td>
+                                        <?php if (session()->get('level') == 1 || session()->get('level') == 2) { ?>
 
-                                <a href="<?= base_url('home/edit_dokter/' . $erwin ->id_dokter) ?>">
-                                 <button class="fa-solid fa-pen"></button>
-                             </a>
+                                            <a href="<?= base_url('home/edit_dokter/' . $erwin->id_dokter) ?>">
+                                                <button class="fa-solid fa-pen"></button>
+                                            </a>
 
-                             <a href="<?= base_url('home/hapus_dokter/' . $erwin ->id_dokter) ?>">
-                                 <button class="fa-solid fa-eraser"></button>
-                             </a>
-                         <?php } ?>
+                                            <a href="<?= base_url('home/hapus_dokter/' . $erwin->id_dokter) ?>">
+                                                <button class="fa-solid fa-eraser"></button>
+                                            </a>
+                                        <?php } ?>
 
-                         <?php if(session()->get('level')==3) { ?>
-                            <a href="<?= base_url('home/booking/' . $erwin ->id_dokter) ?>">
-                                <button type="button" class="btn btn-success">Book</button>
-                            </a>
-                        <?php } ?>
-                    </td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+                                        <?php if (session()->get('level') == 3) { ?>
+                                            <a href="<?= base_url('home/booking/' . $erwin->id_dokter) ?>">
+                                                <button type="button" class="btn btn-success">Book</button>
+                                            </a>
+                                        <?php } ?>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
 </div>
 </div>
 </div>
